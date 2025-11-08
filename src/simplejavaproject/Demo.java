a4edee168833bdfa585e8b97eda4b01c8bf2ae88
Pujitha Vykunta <Pujitha Vykunta@DESKTOP-N79BA49>
2025-11-08 08:06:07 +0530

    my second commit

--- src/simplejavaproject/Demo.java
+++ src/simplejavaproject/Demo.java
@@ -2,6 +2,6 @@ package simplejavaproject;
 
 public class Demo {
   public static void main(String[] args) {
-	System.out.println("hello java......");
+	System.out.println("hello java...87igj uihnkjhk...");
 }
 }
